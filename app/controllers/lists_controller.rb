0b29bbf01b2cc4f479474cbda3c5ea1cc5dd47dd
Ruben Voss <bekertshop@gmail.com>
2022-08-18 14:24:19 +0200

    added frontend for lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,14 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    list = List.new(params[:list])
+    list.save
+  end
+end
