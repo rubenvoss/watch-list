f562378939bdf9923b200a08bb96a78b14226af9
Ruben Voss <bekertshop@gmail.com>
2022-08-19 12:01:07 +0200

    added bookmark#create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,19 +7,32 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = Bookmark.new(params[:bookmark])
-    # if @bookmark.save
-    #   redirect_to list_path
-    # else
+    # @movie = Movie.find(bookmark_params["movie_id"])
+    @list = List.find(params[:list_id])
 
-    # end
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
     # TODO: redirect to
     # @bookmark.list = list
+    raise
 
   end
+
 
   def destroy
     bookmark = Bookmark.find[params[:id]]
     bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # TODO: nest resources
-  resources :lists, only: [:new, :create, :destroy] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
 end
