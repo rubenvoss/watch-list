6376aed96359895ec91bb8ae7821614e0d8a3a1d
Ruben Voss <bekertshop@gmail.com>
2022-08-19 09:04:20 +0200

    .2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,21 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    # raise
+    @list = List.find(params[:list_id])
+    # return an HTML form for creating a new photo
+  end
+
+  def create
+    @bookmark = Bookmark.new(params[:bookmark])
+    # if @bookmark.save
+    #   redirect_to list_path
+    # else
+
+    # end
+    # TODO: redirect to
+    # @bookmark.list = list
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'lists/:id/bookmarks/new', to: "bookmarks#new"
-  get 'lists/:id/bookmarks/:id/destroy', to: "bookmarks#destroy"
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
+  # get 'lists/:id/bookmarks/new', to: "bookmarks#new"
+  # get 'lists/:id/bookmarks/:id/destroy', to: "bookmarks#destroy"
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
-  resources :bookmarks
+  # TODO: nest resources
+  resources :lists, only: [:new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
